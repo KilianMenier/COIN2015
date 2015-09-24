fb54f36270199ae5ca197dabc492aea943056817
Kilian Ménier <kilian.menier@gmx.com>
2015-09-24 16:21:46 +0200

    First official release

--- www/js/XMLDisplay.js
+++ www/js/XMLDisplay.js
@@ -73,9 +73,6 @@ function WriteDay(given_day) {
 }
 
 
-
-
-
 /* Write content of page */
 function WritePlanning() {
     
@@ -105,52 +102,6 @@ function WritePlanning() {
     }
 }
 
-
-
-
-/* Get and write the modal dialogs who gives informations */
-function WriteModal() {
-    
-    // Load XML, get relevant elements
-    xmlDoc = loadxml("planning.xml")
-    wholefile=xmlDoc.getElementsByTagName("event");
-    //alert('modal')
-    
-    for (m=0;m<wholefile.length;m++)
-    {
-        // modalID is used twice, store it to get quicker (not much)
-        // Write a modal for each event
-        var towrite = ""
-        var modalid=wholefile[m].getElementsByTagName("modalID")[0].childNodes[0].nodeValue
-        
-        towrite = '<div id="' + modalid + '" class="modal"> <header class="bar bar-standard bar-nav bar-header"><h1 class="title">' + wholefile[m].getElementsByTagName("title")[0].childNodes[0].nodeValue + '</h1><a class="icon icon-close pull-right" href="#' + modalid + '"></a></header><div class="content"><div class="card"><ul class="table-view"><li class="table-view-cell table-view-divider">' + wholefile[m].getElementsByTagName("WhenWhere")[0].childNodes[0].nodeValue + '</li><li class="table-view-cell">' + wholefile[m].getElementsByTagName("details")[0].childNodes[0].nodeValue + '</li></ul></div>'
-        
-        // If there is a secondary text (secondary WhenWhere and details), include it
-        if ((wholefile[m].getElementsByTagName("WhenWhereSecondary")[0]) && (wholefile[m].getElementsByTagName("detailsSecondary")[0])) { 
-            towrite = towrite + '<div class="card"><ul class="table-view"><li class="table-view-cell table-view-divider">' + wholefile[m].getElementsByTagName("WhenWhereSecondary")[0].childNodes[0].nodeValue + '</li><li class="table-view-cell">' + wholefile[m].getElementsByTagName("detailsSecondary")[0].childNodes[0].nodeValue + '</li></ul></div>'
-        }
-        
-        
-        // With alarms working
-        /*
-        towrite = towrite + '<div class="card"><ul class="table-view"><li class="table-view-cell">Alarme évènement :<div class="toggle '
-        
-        // If alarm is on by default
-        if (wholefile[m].getElementsByTagName("alarmDefault")[0].childNodes[0].nodeValue == "yes") { 
-            towrite = towrite + 'active'
-        }
-        towrite = towrite + '"><div class="toggle-handle"></div></div></li></ul></div>'
-        */
-        
-        
-        towrite = towrite + '</div></div>'
-
-        // Write everything
-        document.write(towrite);
-        //document.body.appendChild(towrite); 
-        //alert('writemodal')
-    }  
-}
 
 
 
